94703929fcaea7da0786fd6ccb6af21008c5c1b1
Joeknow03 <81974924+Joeknow03@users.noreply.github.com>
2022-03-05 14:25:51 +0300

    New file
    Project - 1

---
+++ ContactProject/src/edu/javacourse/contact/dao/ContactDAOFactory.java
@@ -0,0 +1,11 @@
+package edu.javacourse.contact.dao;
+
+/**
+ * Фабрика для создания экземпляра ContactDAO
+ */
+public class ContactDAOFactory 
+{
+    public static ContactDAO getContactDAO() {
+        return new ContactSimpleDAO();
+    }
+}
---
+++ ContactProject/src/edu/javacourse/contact/test/ContactTest.java
@@ -0,0 +1,14 @@
+package edu.javacourse.contact.test;
+
+import edu.javacourse.contact.gui.ContactFrame;
+
+/**
+ * Класс для запуска тестовых вызовов
+ */
+public class ContactTest 
+{
+    public static void main(String[] args) {
+        ContactFrame cf = new ContactFrame();
+        cf.setVisible(true);
+    }
+}
